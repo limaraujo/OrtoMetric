65d179100f14c96bb4aa81c18f88aebf4d1f7329
limaraujo <limafrqa@gmail.com>
2026-04-05 13:01:05 -0300

    migração automática no boot da API

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,9 +1,16 @@
 print("WSGI START")
 
+from flask_migrate import upgrade
+
 from factory import create_app
 
 print("AFTER IMPORT FACTORY")
 
 app = create_app()
+
+print("RUNNING MIGRATIONS")
+with app.app_context():
+	upgrade()
+print("MIGRATIONS OK")
 
 print("APP CREATED")
